f0312298024e765dcbb7e9a0234b2ec8f9676071
lucasandre22ti-boop <lucasandre22.ti@gmail.com>
2025-12-09 23:03:15 -0300

    Add files via upload

--- veículos_adquiridos_cliente.sql
+++ veículos_adquiridos_cliente.sql
@@ -1,7 +1,7 @@
 SELECT
 	C.FIRST_NAME as nome, C.last_name as sobrenome,C.CPF,C.EMAIL,C.CITY,C.STATE,
 	
-	P.BRAND as marca,p.price AS preco,
+	P.BRAND as marca,p.model,p.price AS preco,
 	COUNT(*) AS quantidade
 FROM
 	SALES.FUNNEL AS F
@@ -10,31 +10,34 @@ FROM
 	JOIN SALES.PRODUCTS AS P
 	ON F.PRODUCT_ID = P.PRODUCT_ID
 GROUP BY
-	C.FIRST_NAME,last_name,
+	C.FIRST_NAME,last_name,p.model,
 	P.BRAND,p.price ,C.CPF ,C.EMAIL,C.CITY,C.STATE
 
 limit 10
 
 --VEÍCULOS ADQUIRIDOS POR CLIENTES
 
-"nome","sobrenome","cpf","email","city","state","marca","preco","quantidade"
 
-"MARCELO","DUARTE","29687379920","nobuhiro.duarte464912@gmail.com","GUARULHOS","SP","FIAT","52500","1"
+"nome","sobrenome","cpf","email","city","state","marca","model","preco","quantidade"
 
-"ENZO","RODRIGUES","62333571370","ezo.jonival078868@gmail.com","EMBU DAS ARTES","SP","VOLKSWAGEN","44985","1"
+"MARCELO","DUARTE","61522225618","missilane.duarte816522@gmail.com","SANTA CECÍLIA","SC","VOLKSWAGEN","GOL","51000","1"
 
-"LAURA","OLIVEIRA","34879428220","lauralice.areci045580@gmail.com","SÃO PAULO","SP","FIAT","52500","1"
+"ENZO","RODRIGUES","43428483400","dalmar.ivna063406@gmail.com","GOIÂNIA","GO","VOLKSWAGEN","FOX","45000","1"
 
-"ADEIS","DUARTE","22343560712","adeis.duarte570906@gmail.com","CURITIBA","PR","FORD","35850","1"
+"LAURA","OLIVEIRA","33698078232","arailda.duarte565942@gmail.com","LAGES","SC","CHEVROLET","PRISMA","43500","1"
 
-"ISMAEL","SILVA","74636598474","neidilson.duarte131860@gmail.com","CARAPICUÍBA","SP","CHEVROLET","60000","1"
+"IVETE","DUARTE","04738962348","iveta.shenia341146@gmail.com","SÃO PAULO","SP","HYUNDAI","HB20S","65850","1"
 
-"NESTOR","GONÇALVES","83752241026","laudiseia.duarte407762@gmail.com","SUZANO","SP","FORD","35850","1"
+"ISMAEL","SILVA","80840121068","raid.genai701424@gmail.com","LAGES","SC","VOLKSWAGEN","GOLF","118350","1"
 
-"DANIEL","LIMA","38545029540","noecir.gumercina510406@gmail.com","ITAQUAQUECETUBA","SP","FORD","27000","1"
+"NESTOR","GONÇALVES","09235773792","laurivalda.ludmile750248@gmail.com","SÃO PAULO","SP","CHEVROLET","CORSA","30000","1"
 
-"MOACIR","ALVARES","27877551366","jonieli.lauricio760422@gmail.com","SÃO PAULO","SP","PEUGEOT","47850","1"
+"DANIEL","LIMA","38616956348","suyla.deusidete452962@gmail.com","SOROCABA","SP","VOLKSWAGEN","VOYAGE","44985","1"
 
-"ELIANA","ALVES","17507383282","jeneroso.adeildo948048@gmail.com","JARDINÓPOLIS","SP","CHEVROLET","49350","1"
+"MOACIR","ALVARES","20092917540","netor.ivine645312@gmail.com","SANTO ANDRÉ","SP","CITROEN","C3","54000","1"
 
-"IVAN","MOREIRA","11449959162","iyan.duarte310460@gmail.com","RIBEIRÃO DAS NEVES","MG","VOLKSWAGEN","51000","1"
+"ELIANA","ALVES","62962406110","izaurino.dionario494068@gmail.com","SÃO PAULO","SP","HYUNDAI","HB20S","65850","2"
+
+"IVAN","MOREIRA","73939138604","mecilene.shanon346700@gmail.com","SÃO PAULO","SP","CHEVROLET","SPIN","59850","1"
+
+
